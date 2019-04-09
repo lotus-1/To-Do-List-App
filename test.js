7dbaffaee616e929f1678f79ca589ec8537a72e5
NaremanPro <violette1988@gmail.com>
2019-04-09 15:22:38 +0300

    we added two tests and write the first function

--- test.js
+++ test.js
@@ -1,7 +1,33 @@
 var test = require('tape');
 var logic = require('./logic');
 
-test('Example test', function(t) {
-  t.pass();
+var testTodos = []
+var newTodoTest = {
+  description: "Add to do",
+  done: false
+}
+
+test('Testing Tape', function(t) {
+  t.equal(1, 1, 'Tape is working');
+  t.end();
+});
+
+test('Leaves the original array unchanged', function(t) {
+  var actual = testTodos === logic.addTodo(testTodos, newTodoTest);
+  var expected = false;
+  t.equal(actual, expected, 'Return array is a copy of original array');
+  t.end();
+});
+
+
+test('Testing addTodo funcion', function(t){
+  var actual = logic.addTodo(testTodos, newTodoTest);
+  var expected = [
+    {id: 2,
+    description: "Add to do",
+    done: false
+  }
+  ]
+  t.deepEqual(actual, expected, 'New todo has been added to the todo array');
   t.end();
 });
